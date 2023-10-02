c0d3f32604f2f91fd9d46c032828ba303bf90cfa
Oliverdw1 <oliverdeworm02@gmail.com>
2023-10-02 15:23:59 +0200

    or function

--- or_gate.py
+++ or_gate.py
@@ -4,11 +4,21 @@ def or_gate(a, b):
     elif not a and b:
         return True
     elif a and b:
-        return False
+        return True
     elif not a and not b:
         return False
+
+
+def or_gate2(a, b):
+    return a | b
+
 
 print(or_gate(0,0))
 print(or_gate(0,1))
 print(or_gate(1,0))
 print(or_gate(1,1))
+
+print(or_gate2(0,0))
+print(or_gate2(0,1))
+print(or_gate2(1,0))
+print(or_gate2(1,1))
