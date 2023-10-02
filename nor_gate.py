79c5d4f32ba1a8f9a3cb516a28e1a911865f74f3
Oliverdw1 <oliverdeworm02@gmail.com>
2023-10-02 16:13:20 +0200

    xor gate

--- nor_gate.py
+++ nor_gate.py
@@ -6,7 +6,7 @@ def nor_gate(a, b):
 # return not(a or b)
 
 
-print(nor_gate(0,0))
-print(nor_gate(0,1))
-print(nor_gate(1,0))
-print(nor_gate(1,1))
+print(nor_gate(0, 0))
+print(nor_gate(0, 1))
+print(nor_gate(1, 0))
+print(nor_gate(1, 1))
---
+++ xor_gate.py
@@ -0,0 +1,11 @@
+def xor_gate(a, b):
+    if a == b:
+        return False
+    else:
+        return True
+
+
+print(xor_gate(0,0))
+print(xor_gate(0,1))
+print(xor_gate(1,0))
+print(xor_gate(1,1))
